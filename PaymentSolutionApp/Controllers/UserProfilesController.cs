671bdebc1e0d81402965f786a3435e2250c089aa
olabisi olawale A <ejiolawale4@gmail.com>
2020-02-20 04:23:01 +0100

    paymentsolution asp.net MVC c#

---
+++ PaymentSolutionApp/Controllers/UserProfilesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PaymentSolutionApp.Models;
+
+namespace PaymentSolutionApp.Controllers
+{
+    public class UserProfilesController : Controller
+    {
+        private paymentsolutionEntities db = new paymentsolutionEntities();
+
+        // GET: UserProfiles
+        public ActionResult Index()
+        {
+            return View(db.UserProfiles.ToList());
+        }
+
+        // GET: UserProfiles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProfile userProfile = db.UserProfiles.Find(id);
+            if (userProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserProfiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,UserName,FirstName,LastName,Address,City,Country,Email,Company,AboutMe,PostalCode")] UserProfile userProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProfiles.Add(userProfile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProfile userProfile = db.UserProfiles.Find(id);
+            if (userProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userProfile);
+        }
+
+        // POST: UserProfiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,UserName,FirstName,LastName,Address,City,Country,Email,Company,AboutMe,PostalCode")] UserProfile user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.sp_UpdateUserProfile(user.UserID,user.UserName,user.FirstName,user.LastName,user.Address,user.City,user.Country,user.Email,user.Company,user.AboutMe,user.PostalCode);
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: UserProfiles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProfile userProfile = db.UserProfiles.Find(id);
+            if (userProfile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userProfile);
+        }
+
+        // POST: UserProfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            db.sp_DeleteUserProfile(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
